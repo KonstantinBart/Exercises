8bda13b33e0c3d556310ac16ba9a783fcde62de5
KonstantinBart <kostyamagi@mail.ru>
2020-10-23 00:13:29 +0300

    First commit

---
+++ ReverseInteger/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace ReverseInteger
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Reverse(123));
+			Console.WriteLine(Reverse(-123));
+			Console.WriteLine(Reverse(120));
+			Console.ReadLine();
+		}
+
+		public static int Reverse(int x)
+		{
+			var isNegative = x < 0;
+			var numberAsString = x.ToString(CultureInfo.InvariantCulture)
+				.TrimEnd('0')
+				.TrimStart('-')
+				.Reverse();
+			var resultString = string.Empty;
+			for (int i = 0; i < numberAsString.Count(); i++)
+			{
+				resultString += numberAsString.ElementAt(i);
+			}
+			var resultInt = 0;
+			try
+			{
+				resultInt = Convert.ToInt32(resultString);
+			}
+			catch { }
+			return isNegative ? resultInt * -1 : resultInt;
+		}
+	}
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TwoSum
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] result = TwoSum(new[] { 2, 7, 11, 15 }, 9);
+			Console.ReadLine();
+		}
+
+		public static int[] TwoSum(int[] nums, int target)
+		{
+			for (int i = 0; i <= nums.Length - 1; i++)
+			{
+				for (int j = i + 1; j < nums.Length; j++)
+				{
+					if (nums[i] + nums[j] == target)
+						return new[] { i, j };
+				}
+			}
+			throw new InvalidOperationException("There is no decision for number " + target);
+		}
+	}
+}
