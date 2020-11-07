24a9c9a3b49919494fdfa88de8614e87c4b260d1
KonstantinBart <kostyamagi@mail.ru>
2020-11-08 00:22:32 +0300

    Changed from array to numbers calculations

--- PalindromeNumber/Solution.cs
+++ PalindromeNumber/Solution.cs
@@ -1,34 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace PalindromeNumber
+﻿namespace PalindromeNumber
 {
     public class Solution
     {
         public static bool IsPalindrome(int x)
         {
             if (x < 0) return false;
-            if (x < 10) return true;
-            var array = new List<int>();
-            while(x != 0)
+            var sourceValue = x;
+            int result = 0;
+            while(x > 0)
             {
-                array.Add(x % 10);
+                result = result * 10 + x % 10;
                 x /= 10;
             }
-            if(!array.Any() || array.Count < 2)
-            {
-                return false;
-            }
-            while(array.Count >= 2)
-            {
-                var firstElement = array.First();
-                var lastElement = array.Last();
-                if (firstElement != lastElement)
-                    return false;
-                array.RemoveAt(0);
-                array.RemoveAt(array.Count - 1);
-            }
-            return true;
+            return sourceValue == result;
         }
     }
 }
